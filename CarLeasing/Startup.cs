2ec62a8aad7b8e9d83c6b461be65c6226ab62c32
KlichnikovDA <spacerus99@gmail.com>
2020-12-12 21:28:21 +0500

    Добавьте файлы проекта.

---
+++ CarLeasing/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CarLeasing.Startup))]
+namespace CarLeasing
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
